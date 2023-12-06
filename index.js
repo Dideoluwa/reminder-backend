9bd0376a8ec03ff526704417cf71390c3e3706d5
Dideoluwa Oni <darasimidideoluwa@gmail.com>
2023-12-06 21:07:40 +0100

    commit

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ const triggerSendMail = async () => {
 // Set the cron expression to run the function every minute
 // const task = cron.schedule("0 */6 * * *", triggerSendMail);
 
-const task = cron.schedule("*/2 * * * *", triggerSendMail);
+const task = cron.schedule("* * * * *", triggerSendMail);
 
 app.get("/", (req, res) => {
   res.send("hello world");
