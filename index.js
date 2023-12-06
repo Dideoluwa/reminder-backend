6e1031f7187419268591fc4554d8be9d56c95a42
Dideoluwa Oni <darasimidideoluwa@gmail.com>
2023-12-06 10:32:07 +0100

    try fixing time to 11am

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ const triggerSendMail = async () => {
 };
 
 // Set the cron expression to run the function every minute
-const task = cron.schedule("0 0,6,9,12,18 * * *", triggerSendMail);
+const task = cron.schedule("0 0,6,11,12,18 * * *", triggerSendMail);
 
 app.get("/", (req, res) => {
   res.send("hello world");
